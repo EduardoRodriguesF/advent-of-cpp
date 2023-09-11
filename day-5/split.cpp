8b4fade931a1e2734d8f0a2cdf30bd7c8e4ac70e
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-09-11 20:01:34 -0300

    Optmize split function

--- day-5/split.cpp
+++ day-5/split.cpp
@@ -4,16 +4,20 @@ std::vector<std::string> split(const std::string &str, const char &separator) {
 	std::vector<std::string> result;
 	std::string entry;
 
-	for (int i = 0; i <= str.size(); i++) {
+	for (int i = 0; i < str.size(); i++) {
 		char c = str[i];
 
-		if (c == separator || i == str.size()) {
+		if (c == separator) {
 			result.push_back(entry);
 			entry = "";
 			continue;
 		}
 
 		entry += c;
+	}
+
+	if (!entry.empty()) {
+		result.push_back(entry);
 	}
 
 	return result;
